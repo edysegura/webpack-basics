829019cc9b710fd6dc0d7817bc9e7b578eb69fac
Edy Segura <ednardo@inatel.br>
2017-02-27 11:57:12 -0300

    Added README.md to command-line example

---
+++ command-line/README.md
@@ -0,0 +1,17 @@
+# Webpack command-line
+
+This example is using ```webpack``` with the command-line via NPM scripts.
+
+## How to run it
+
+Fist install the dependencies
+
+```
+npm install
+```
+
+and then
+
+```
+npm start
+```
--- command-line/src/js/main.js
+++ command-line/src/js/main.js
@@ -3,4 +3,4 @@ import { map } from 'lodash';
 const square = (number) => number * number; 
 
 console.log("------------------ Square ------------------");
-console.log(map([1,2,3,4,5,6], square));
+console.log( map([1,2,3,4,5,6], square) );
