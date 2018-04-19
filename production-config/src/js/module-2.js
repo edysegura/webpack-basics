655761619f38a1cd26b65e6a9768eb51c854aac5
Edy Segura <ednardo@inatel.br>
2018-04-18 23:08:56 -0300

    Refactoring

--- production-config/src/js/module-2.js
+++ production-config/src/js/module-2.js
@@ -1,7 +1,8 @@
-import {groupBy} from 'lodash/collection';
+import { groupBy } from 'lodash/collection';
 import people from './people';
 
 const managerGroups = groupBy(people, 'manager');
-
 const p = document.querySelector('output');
-p.innerHTML = '<pre>' + JSON.stringify(managerGroups, null, 2) + '</pre>';
+const jsonText = JSON.stringify(managerGroups, null, 2);
+
+p.innerHTML = `<pre>${jsonText}</pre>`;
--- production-config/src/js/people.js
+++ production-config/src/js/people.js
@@ -1,28 +1,28 @@
 const people = [
-    {
-        manager: 'Jen',
-        name: 'Bob'
-    },
-    {
-        manager: 'Jen',
-        name: 'Sue'
-    },
-    {
-        manager: 'Bob',
-        name: 'Shirley'
-    },
-    {
-        manager: 'Bob',
-        name: 'Terrence'
-    },
-    {
-        manager: 'Liao',
-        name: 'Tomazelli'
-    },
-    {
-        manager: 'Liao',
-        name: 'Edy Segura'
-    }
+  {
+    manager: 'Jen',
+    name: 'Bob'
+  },
+  {
+    manager: 'Jen',
+    name: 'Sue'
+  },
+  {
+    manager: 'Bob',
+    name: 'Shirley'
+  },
+  {
+    manager: 'Bob',
+    name: 'Terrence'
+  },
+  {
+    manager: 'Liao',
+    name: 'Tomazelli'
+  },
+  {
+    manager: 'Liao',
+    name: 'Edy Segura'
+  }
 ];
 
 export default people;
